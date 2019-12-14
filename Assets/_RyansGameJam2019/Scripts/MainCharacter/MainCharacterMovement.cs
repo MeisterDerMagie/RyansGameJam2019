103d9f384f4e12c061cda2f6684509d6962a8005
Martin_Home <martin.kloeckener@gmail.com>
2019-12-14 20:26:54 +0100

    improved movement

--- Assets/_RyansGameJam2019/Scripts/MainCharacter/MainCharacterMovement.cs
+++ Assets/_RyansGameJam2019/Scripts/MainCharacter/MainCharacterMovement.cs
@@ -12,23 +12,31 @@ public class MainCharacterMovement : MonoBehaviour
     [SerializeField, BoxGroup("Settings"), Required] private float stopFactor = 0.2f;
     [SerializeField, FoldoutGroup("References"), Required, ReadOnly] private Rigidbody2D rigidbody;
 
+    
     private void FixedUpdate()
     {
         var horizontal = Input.GetAxis("Horizontal");
         var vertical   = Input.GetAxis("Vertical");
 
+        /*
         if (Mathf.Abs(horizontal) < 0.05f && Mathf.Abs(vertical) < 0.05f) //slow down if nothing is pressed
         {
             rigidbody.AddForce(-stopFactor * rigidbody.velocity);
             return;
-        } 
+        }
+        */
         
         var forceX = Time.fixedDeltaTime * speed * horizontal;
         var forceY = Time.fixedDeltaTime * speed * vertical;
         forceX = Mathf.Clamp(forceX, -maxSpeed, maxSpeed);
         forceY = Mathf.Clamp(forceY, -maxSpeed, maxSpeed);
         
-        rigidbody.AddForce(new Vector2(forceX, forceY));
+        //rigidbody.velocity = new Vector2(forceX, forceY);
+        
+        rigidbody.AddForce(new Vector2(forceX, forceY), ForceMode2D.Force);
+        
+        //if (rigidbody.velocity.magnitude > maxSpeed)
+        //    rigidbody.velocity = maxSpeed * rigidbody.velocity.normalized;
     }
 
 
