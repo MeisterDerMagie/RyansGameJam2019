0d00b3cff61d0d6b536e47ff0578327f5f7f4da5
Martin_Home <martin.kloeckener@gmail.com>
2019-12-15 16:07:31 +0100

    added Doozy and Main Menu

---
+++ Assets/_RyansGameJam2019/Scripts/General/QuitGame.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RGJ{
+public class QuitGame : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape)) Quit();
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
+}
